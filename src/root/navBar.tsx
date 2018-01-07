ccdc9dca88ccfbced1c06c6a5b36052a81020087
James Ross <jross3@alumni.stanford.edu>
2018-01-07 12:20:08 -0800

    a couple small things

--- src/root/navBar.tsx
+++ src/root/navBar.tsx
@@ -1,14 +1,17 @@
 /**
  * External
  */
-import { Classes } from "@blueprintjs/core";
+import { Classes, Icon, IconClasses } from "@blueprintjs/core";
 import * as React from "react";
 
 export class NavBar extends React.PureComponent<{}, {}> {
 
     public render() {
         return (
-            <div className={Classes.NAVBAR}>Hello, James</div>
+            <div className={Classes.NAVBAR}>
+                <Icon iconName={IconClasses.USER} iconSize={20}/>
+                Hello, James
+            </div>
         );
     }
 }
